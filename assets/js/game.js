697235e955b5f7d83bd53ba7badeb8fb19aa3b9b
emccar22 <e.mccarthy2285@gmail.com>
2021-11-07 23:27:46 -0500

    randomize fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,7 +26,13 @@ var fightOrSkip = function(){
 }
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
   while (playerInfo.health > 0 && enemy.health > 0) {
+   if (isPlayerTurn) {
     if (fightOrSkip()) {
       break;
     }
@@ -68,6 +74,9 @@ var fight = function(enemy) {
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
+  }
+  // switch order for next round
+  isPlayerTurn = !isPlayerTurn;
   }
 };
 
